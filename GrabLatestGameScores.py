2127c1c5582981962d731bcfa4e1666740933fa9
jabar <jabari_mf@hotmail.com>
2022-09-19 17:38:01 -0500

    Game Scores finished, working on on stats grabber

--- GrabLatestGameScores.py
+++ GrabLatestGameScores.py
@@ -11,6 +11,7 @@ import matplotlib.pyplot as plt
 
 # URL of page
 url = 'https://www.pro-football-reference.com/boxscores/'
+
 # Open URL and pass to BeautifulSoup
 html = urlopen(url)
 stats_page = BeautifulSoup(html, 'html.parser')
@@ -20,18 +21,30 @@ stats_page = BeautifulSoup(html, 'html.parser')
 #print(stats_page.findAll('tr'))
 
 
-def GrabGameScores():
+def GrabLatestGameScores():
 
     game_scores  = stats_page.find_all('table',class_='teams')
-    #print(game_scores)
+    game_number = 1
+
 
     for game_score in game_scores:
-        loser_team = game_score.find_next(class_="loser")
-        winner_team = game_score.find_next(class_="winner")
-        print(winner_team.text.strip())
-        print(loser_team.text.strip())
+
+        losing_team = game_score.find(class_="loser")
+        winning_team = game_score.find(class_="winner")
+        print(f"Game {game_number}")
+        print(f"{winning_team.contents[1].text.strip()} {winning_team.contents[3].text}")
+        print(f"{losing_team.contents[1].text.strip()} {losing_team.contents[3].text}")
+
+        game_number = game_number+1
+
+def GrabStats():
+
+    player_stats = stats_page.find_all('table',class_='stats')
+
+    for player_stat in player_stats:
+        top_passer = player_stats.find()
 
 
+GrabLatestGameScores()
 
 
-#stats_page.findAll(name)
