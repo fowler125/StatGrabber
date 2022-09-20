04279cbd509ce27fc176ae84681a40a2811cb2cc
jabar <jabari_mf@hotmail.com>
2022-09-20 13:10:01 -0500

    Export to txt finished

--- GrabLatestGameScores.py
+++ GrabLatestGameScores.py
@@ -16,35 +16,107 @@ url = 'https://www.pro-football-reference.com/boxscores/'
 html = urlopen(url)
 stats_page = BeautifulSoup(html, 'html.parser')
 
-#print(stats_page.prettify())
-#print(stats_page.tr)
-#print(stats_page.findAll('tr'))
+# print(stats_page.prettify())
+# print(stats_page.tr)
+# print(stats_page.findAll('tr'))
+
+Dict = {
+    'Arizona Cardinals': 'ARI',
+    'Atlanta Falcons': 'ATL',
+    'Baltimore Ravens': 'BAL',
+    'Buffalo Bills': 'BUF',
+    'Carolina Panthers': 'CAR',
+    'Chicago Bears': 'CHI',
+    'Cincinnati Bengals': 'CIN',
+    'Cleveland Browns':'CLE',
+    'Dallas Cowboys':'DAL',
+    'Denver Broncos':'DEN',
+    'Detroit Lions':'DET',
+    'Green Bay Packers':'GB',
+    'Houston Texans':'HOU',
+    'Indianapolis Colts':'IND',
+    'Jacksonville Jaguars': 'JAC',
+    'Kansas City Chiefs': 'KC',
+    'Las Vegas Raiders':'LV',
+    'Los Angeles Chargers': 'CHA',
+    'Los Angeles Rams':'LAR',
+    'Miami Dolphins':'MIA',
+    'Minnesota Vikings':'MIN',
+    'New England Patriots':'NE',
+    'New Orleans Saints':'NO',
+    'New York Giants':'NYG',
+    'New York Jets':'NYJ',
+    'Philadelphia Eagles':'PHI',
+    'Pittsburgh Steelers':'PIT',
+    'San Francisco 49ers':'SF',
+    'Seattle Seahawks':'SEA',
+    'Tampa Bay Buccaneers':'TB',
+    'Tennessee Titans':'TEN',
+    'Washington Commanders':'WAS'
+}
 
 
 def GrabLatestGameScores():
-
-    game_scores  = stats_page.find_all('table',class_='teams')
+    game_scores = stats_page.find_all('table', class_='teams')
     game_number = 1
-
 
     for game_score in game_scores:
 
         losing_team = game_score.find(class_="loser")
         winning_team = game_score.find(class_="winner")
-        print(f"Game {game_number}")
-        print(f"{winning_team.contents[1].text.strip()} {winning_team.contents[3].text}")
-        print(f"{losing_team.contents[1].text.strip()} {losing_team.contents[3].text}")
 
-        game_number = game_number+1
+        WinnerName = winning_team.contents[1].text.strip()
+        LoserName = losing_team.contents[1].text.strip()
 
-def GrabStats():
+        file1 = open(f"GameScores/{Dict[WinnerName]}v{Dict[LoserName]}", "w+")
 
-    player_stats = stats_page.find_all('table',class_='stats')
+        #file1.writelines(f"Game: {WinnerName} v {LoserName} \n")
+        #print(f"Game: {WinnerName} v {LoserName}")
 
-    for player_stat in player_stats:
-        top_passer = player_stats.find()
+        print(f"{winning_team.contents[1].text.strip()} - {winning_team.contents[3].text}")
+        file1.writelines(f" {winning_team.contents[1].text.strip()} - {winning_team.contents[3].text} \n")
+
+        print(f"{losing_team.contents[1].text.strip()} - {losing_team.contents[3].text}")
+        file1.writelines(f" {losing_team.contents[1].text.strip()} - {losing_team.contents[3].text} \n")
+
+        GrabStats(WinnerName, LoserName, game_number - 1)
+
+        game_number = game_number + 1
+
+
+
+        file1.close()
+
+
+
+def GrabStats(winner,loser,stat):
+    player_stats = stats_page.find_all('table', class_='stats')
+    # print(player_stats)
+    TOP_PASSER_LABEL = player_stats[stat].contents[1].contents[1].contents[1].text.strip()
+    TOP_PASSER_NAME = player_stats[stat].contents[1].contents[1].contents[3].text.strip()
+    TOP_PASSER_YDS = player_stats[stat].contents[1].contents[1].contents[5].text.strip()
+
+    TOP_RUSHER_LABEL = player_stats[stat].contents[1].contents[3].contents[1].text.strip()
+    TOP_RUSHER_NAME = player_stats[stat].contents[1].contents[3].contents[3].text.strip()
+    TOP_RUSHER_YDS = player_stats[stat].contents[1].contents[3].contents[5].text.strip()
+
+    TOP_RECEIVER_LABEL = player_stats[stat].contents[1].contents[5].contents[1].text.strip()
+    TOP_RECEIVER_NAME = player_stats[stat].contents[1].contents[5].contents[3].text.strip()
+    TOP_RECEIVER_YDS = player_stats[stat].contents[1].contents[5].contents[5].text.strip()
+
+    PlayerScores = open(f"TopPlayerStats/{Dict[winner]}v{Dict[loser]}stats", "w+")
+
+    print(f"{TOP_PASSER_LABEL} {TOP_PASSER_NAME} {TOP_PASSER_YDS}")
+    PlayerScores.writelines(f"{TOP_PASSER_LABEL} {TOP_PASSER_NAME} {TOP_PASSER_YDS}\n")
+
+    print(f"{TOP_RUSHER_LABEL} {TOP_RUSHER_NAME} {TOP_RUSHER_YDS} ")
+    PlayerScores.writelines(f"{TOP_RUSHER_LABEL} {TOP_RUSHER_NAME} {TOP_RUSHER_YDS}\n")
+
+    print(f"{TOP_RECEIVER_LABEL} {TOP_RECEIVER_NAME} {TOP_RECEIVER_YDS} ")
+    PlayerScores.writelines(f"{TOP_RECEIVER_LABEL} {TOP_RECEIVER_NAME} {TOP_RECEIVER_YDS}\n")
+
+    PlayerScores.close()
 
 
 GrabLatestGameScores()
-
-
+#GrabStats('KAN','LAC')
